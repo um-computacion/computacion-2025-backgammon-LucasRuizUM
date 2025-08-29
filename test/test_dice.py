25410672425ae83a7016f241b3dbf4294799cf19
Lucas Ruiz <li.ruiz@alumno.um.edu.ar>
2025-08-28 22:16:30 -0300

    arreglo de test dice

--- test/test_dice.py
+++ test/test_dice.py
@@ -1,13 +1,25 @@
 import unittest
-from computacion_2025_backgammon_LucasRuizUM.core.dice import Dice
+from core.dice import Dice
 
 class TestDice(unittest.TestCase):
     def test_roll(self):
         dice = Dice()
         values = dice.roll()
+        self.assertIsInstance(values, tuple)
         self.assertEqual(len(values), 2)
-        self.assertTrue(1 <= values [0] <= 6)
-        self.assertTrue(1 <= values[1] <= 6)
+
+        for v in values:
+            self.assertIsInstance(v, int)
+            self.assertGreaterEqual(v, 1)
+            self.assertLessEqual(v, 6)
+
+    def test_multiples_rolls(self):
+        dice = Dice()
+        for _ in range(1000):
+            values = dice.roll()
+            for v in values:
+                self.assertGreaterEqual(v, 1)
+                self.assertLessEqual(v, 6)
 
 if __name__ == "__main__":
     unittest.main()
